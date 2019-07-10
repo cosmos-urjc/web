64871d0a364a092ab39646df26a85a9d53d32cf4
rubenayla <ruben.jimenezmejias@gmail.com>
2019-07-10 13:56:01 +0200

    error in register function solved

--- login.js
+++ login.js
@@ -88,8 +88,8 @@ function login(){
 }
 function register(){
 
-	var email = document.getElementById("email-input").value;
-	var pwd = document.getElementById("pwd-input").value;
+	var email = document.getElementById("email-input-login").value;
+	var pwd = document.getElementById("pwd-input-login").value;
 
 	firebase.auth().createUserWithEmailAndPassword(email, pwd).catch(function(error) {
 		// Handle Errors here.
