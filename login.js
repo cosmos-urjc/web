662583ae5e2ace152061da54bdc73f8f8d2b2c1c
rubenayla <ruben.jimenezmejias@gmail.com>
2019-07-10 13:47:21 +0200

    new login system

--- login.js
+++ login.js
@@ -1,26 +1,71 @@
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {
-		// User is signed in.
+		// User is signed in, so show the loggedin-div
 		document.getElementById("login-div").style.display = "none";
 		document.getElementById("loggedin-div").style.display = "block";
-
-		// Get user data to show
+		
+		// Get user auth and DB data
 		var user = firebase.auth().currentUser;
-		if (user != null){
-			var name = user.displayName;
-			var email = user.email;
-			var photoURL= user.photoURL;
-			var description = user.description;
-			var emailVerified= user.emailVerified;
-			var uid= user.uid;
 
+		var name = user.displayName;
+		var email = user.email;
+		var photoURL= user.photoURL;
+		var emailVerified = user.emailVerified;
+		var uid = user.uid;
 
-
-			document.getElementById("user-welcome").innerHTML = "Bienvenido " + email;
-			document.getElementById("user-info").innerHTML = "Name: " + name
-			+ "<br>" + "<img src=\"" + photoURL + "\">";
-
+		// Welcome message
+		if (name != null) {
+			document.getElementById("user-welcome").innerHTML = "Bienvenido/a " + name;
+		} else {
+			document.getElementById("user-welcome").innerHTML = "Bienvenido/a " + email;
 		}
+		
+		// Imprimir datos de perfil (Auth)
+		if (user.photoURL != null){
+			document.getElementById("profile-image").innerHTML = "<img src=\"" + photoURL + "\">";
+		} else {
+			// In case there was an image before and the user has 
+			// removed it. It must go away.
+			document.getElementById("profile-image").innerHTML = null;
+		}
+		document.getElementById("name-input").value = name;
+		document.getElementById("email-data").innerHTML = email;
+		document.getElementById("photourl-input").value = user.photoURL;
+
+		// Imprimir datos de DB
+		/*
+		//Realtime
+		firebase.database().ref('users/' + user.uid + '/bio').on('value',function(snapshot){
+			document.getElementById("bio-input").value = snapshot.val();
+		});
+		*/
+		firebase.database().ref('users/' + user.uid).once('value').then(function(snapshot){
+			if (snapshot.val().username != undefined){
+				document.getElementById("username-input").value = snapshot.val().username;
+			} else {
+				document.getElementById("username-input").value = null;
+			}
+
+			if (snapshot.val().surname != undefined){
+				document.getElementById("surname-input").value = snapshot.val().surname;
+			} else {
+				document.getElementById("surname-input").value = null;
+			}
+
+			if (snapshot.val().bio != undefined){
+				document.getElementById("bio-input").value = snapshot.val().bio;
+			} else {
+				document.getElementById("surname-input").value = null;
+			}
+
+			if (snapshot.val().freetext != undefined){
+				document.getElementById("freetext-input").value = snapshot.val().freetext;
+			} else {
+				document.getElementById("freetext-input").value = null;
+			}
+			// TODO: MAKE THIS SHORT AND ELEGANT
+		});
+
 	} else {
 		// No user is signed in.
 		document.getElementById("login-div").style.display = "block";
@@ -28,10 +73,10 @@ firebase.auth().onAuthStateChanged(function(user) {
 	}
 });
 
-function login(){
 
-	var email = document.getElementById("email-input").value;
-	var pwd = document.getElementById("pwd-input").value;
+function login(){
+	var email = document.getElementById("email-input-login").value;
+	var pwd = document.getElementById("pwd-input-login").value;
 
 	firebase.auth().signInWithEmailAndPassword(email, pwd).catch(function(error) {
 		// Handle Errors here.
@@ -40,9 +85,7 @@ function login(){
 
 		alert("Error: " + errorMessage);
 	});
-
 }
-
 function register(){
 
 	var email = document.getElementById("email-input").value;
@@ -56,11 +99,9 @@ function register(){
 		alert("Error: " + errorMessage);
 	});
 }
-
 function logout(){
 	firebase.auth().signOut();
 }
-
 function google_login(){
 	var provider = new firebase.auth.GoogleAuthProvider();
 
@@ -82,7 +123,6 @@ function google_login(){
 		alert(error);
 	});
 }
-
 // function twitter_login(){
 // 	var provider = new firebase.auth.TwitterAuthProvider();
 
@@ -104,9 +144,7 @@ function google_login(){
 // 		alert(error);
 // 	});
 // }
-
 function forgotten(){
-
 	var email = document.getElementById("email-input").value;
 	var auth = firebase.auth();
 
@@ -116,11 +154,22 @@ function forgotten(){
 		alert(error);
 	});
 }
-
-function update(){
-
+function delete_account() {
 	var user = firebase.auth().currentUser;
 
+	user.delete().then(function() {
+		// User deleted.
+	}).catch(function(error) {
+		// An error happened.
+		alert(error);
+	});
+}
+
+
+function update(){
+	var user = firebase.auth().currentUser;
+	
+	// Update auth data (name and photoURL)
 	user.updateProfile({
 		displayName: document.getElementById("name-input").value,
 		photoURL: document.getElementById("photourl-input").value
@@ -131,15 +180,15 @@ function update(){
 		// An error happened.
 		// alert(error);
 	});
-}
 
-function delete_account() {
-	var user = firebase.auth().currentUser;
-
-	user.delete().then(function() {
-		// User deleted.
-	}).catch(function(error) {
-		// An error happened.
-		alert(error);
+	// Profile data to DB
+	firebase.database().ref('users/' + user.uid).set({
+		email: user.email,
+		username: document.getElementById("username-input").value,
+		name: user.displayName,
+		surname: document.getElementById("surname-input").value,
+		photo_url: user.photoURL,
+		bio: document.getElementById("bio-input").value,
+		freetext: document.getElementById("freetext-input").value
 	});
 }
